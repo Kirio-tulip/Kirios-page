d4ee10bba06102a010569ad30b9f75e0ecee90f3
Kirio-tulip <angeltrhds@gmail.com>
2025-09-14 17:34:27 +0500

    About me page

---
+++ Script.js
@@ -0,0 +1,14 @@
+function About() {
+  window.location.href = "about.html";
+}
+
+
+function Socials() {
+  alert("⚙️ Socials coming soon!");
+}
+
+function Interests() {
+  alert("❓ Interests still not here.. ");
+}
+
+
